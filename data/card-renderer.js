3a82fd17b362a0228168648c2804dba4e44207a7
Brian Rainey <raineyb@gmail.com>
2014-11-28 14:53:36 -0600

    v1.0.2 close panel on card click

--- data/card-renderer.js
+++ data/card-renderer.js
@@ -1,7 +1,11 @@
-var cardImage = document.getElementById("cardImage");
-var cardLink = document.getElementById("cardLink");
+var cardImage = document.getElementById('cardImage');
+var cardLink = document.getElementById('cardLink');
 
-self.port.on("show-card", function(cardSource, cardUrl) {
+self.port.on('show-card', function(cardSource, cardUrl) {
   cardImage.src = cardSource;
   cardLink.href = cardUrl;
+});
+
+cardLink.addEventListener('click', function(ev){
+    self.port.emit('card-clicked');
 });
