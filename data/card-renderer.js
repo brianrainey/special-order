acfdb81b48b9d1600a517b9a39d34e3b82c34c43
Brian Rainey <brian@rainey.us>
2014-12-14 17:06:54 -0500

    v1.1.2 Add preference for auto-display, Add loading image to prevent old card for flashing before current card is displayed.

--- data/card-renderer.js
+++ data/card-renderer.js
@@ -1,11 +1,11 @@
 var cardImage = document.getElementById('cardImage');
 var cardLink = document.getElementById('cardLink');
 
-self.port.on('show-card', function(cardSource, cardUrl) {
+self.port.on('load-card', function(cardSource, cardUrl) {
   cardImage.src = cardSource;
   cardLink.href = cardUrl;
 });
 
 cardLink.addEventListener('click', function(ev){
-    self.port.emit('card-clicked');
+  self.port.emit('card-clicked');
 });
