9fe75b66dd20df4c5d30b0775b5194b691afda01
Brian Rainey <raineyb@gmail.com>
2014-12-31 10:18:20 -0600

    Fix intermittent NPE and add support for curly quotes / apostrophes.

--- data/link-catcher.js
+++
@@ -1,8 +0,0 @@
-var links = document.querySelectorAll('[href*="netrunnerdb.com"]')
-
-for each (link in links) {
-    link.onclick = function(mouseEvent) {
-        console.log(mouseEvent);
-        return false;
-    }
-}
